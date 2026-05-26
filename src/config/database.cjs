b851a47b899a05cf5edf0cebae4d4747279b9cff
Alan <alandsantos54@gmail.com>
2026-05-26 19:08:00 -0300

    conexão de banco de dados e a criação de models

---
+++ src/config/database.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: '642581',
+  database: 'magazine-db',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
