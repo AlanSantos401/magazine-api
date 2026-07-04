d6cb93842c86aba66117604b41f98f3e92ea8f00
Alan <alandsantos54@gmail.com>
2026-07-04 17:20:51 -0300

    Atualização

--- src/config/database.cjs
+++ src/config/database.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'admin',
-  password: '642581',
-  database: 'magazine-db',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+
   define: {
     timestamps: true,
     underscored: true,
